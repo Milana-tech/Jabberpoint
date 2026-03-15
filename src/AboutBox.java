bad77f2d71587367f03828c4ad3dfcb659834e39
Milana <milana.doborjginidze@student.nhlstenden.com>
2026-03-15 23:18:43 +0100

    Clean up AboutBox class

--- src/AboutBox.java
+++ src/AboutBox.java
@@ -1,21 +1,27 @@
 import javax.swing.*;
 import java.awt.*;
 
+/**
+ * AboutBox shows the application's about dialog.
+ *
+ * SRP: Responsible only for displaying the about message.
+ */
+
 public class AboutBox
 {
+    private static final String TITLE = "About JabberPoint";
+    private static final String MESSAGE =
+            "JabberPoint is a primitive slide-show program in Java(tm).\n" +
+                    "It is freely copyable as long as you keep this notice and\n" +
+                    "the splash screen intact.\n" +
+                    "Copyright (c) 1995-1997 by Ian F. Darwin, ian@darwinsys.com.\n" +
+                    "Adapted by Gert Florijn (version 1.1) and Sylvia Stuurman\n" +
+                    "(version 1.2 and higher) for the Open University of the\n" +
+                    "Netherlands, 2002 -- now.\n" +
+                    "Author's version available from http://www.darwinsys.com/";
+
     public static void show(Frame parent)
     {
-        JOptionPane.showMessageDialog(parent,
-                "JabberPoint is a primitive slide-show program in Java(tm). It\n" +
-                        "is freely copyable as long as you keep this notice and\n" +
-                        "the splash screen intact.\n" +
-                        "Copyright (c) 1995-1997 by Ian F. Darwin, ian@darwinsys.com.\n" +
-                        "Adapted by Gert Florijn (version 1.1) and " +
-                        "Sylvia Stuurman (version 1.2 and higher) for the Open" +
-                        "University of the Netherlands, 2002 -- now." +
-                        "Author's version available from http://www.darwinsys.com/",
-                "About JabberPoint",
-                JOptionPane.INFORMATION_MESSAGE
-        );
+        JOptionPane.showMessageDialog(parent, MESSAGE, TITLE, JOptionPane.INFORMATION_MESSAGE);
     }
 }
