c5a9e790fa1a14534ef23ad23ae6ca35d7d27906
Rodrigo <rodricardoso7@outlook.com>
2026-03-19 23:14:12 +0100

    Fixed argument errors in slide.append from DemoPresentationLoader

--- src/accessor/DemoPresentationLoader.java
+++ src/accessor/DemoPresentationLoader.java
@@ -3,6 +3,7 @@ package accessor;
 import presentation.Presentation;
 import slide.BitmapItem;
 import slide.Slide;
+import slide.SlideComponentFactoryManager;
 
 import java.io.IOException;
 
@@ -14,6 +15,7 @@ import java.io.IOException;
  */
 public class DemoPresentationLoader extends Accessor
 {
+    private static final SlideComponentFactoryManager FACTORY_MANAGER = new SlideComponentFactoryManager();
 
     @Override
     public void loadPresentationFromFile(Presentation presentation, String filename)
@@ -34,14 +36,15 @@ public class DemoPresentationLoader extends Accessor
     {
         Slide slide = new Slide();
         slide.setTitle("JabberPoint");
-        slide.append(1, "The Java presentation.Presentation Tool");
-        slide.append(2, "Copyright (c) 1996-2000: Ian Darwin");
-        slide.append(2, "Copyright (c) 2000-now: Gert Florijn and Sylvia Stuurman");
-        slide.append(4, "Running JabberPoint without a filename shows this demo");
-        slide.append(1, "Navigation:");
-        slide.append(3, "Next slide: PgDn or Enter");
-        slide.append(3, "Previous slide: PgUp or up-arrow");
-        slide.append(3, "Quit: q or Q");
+        slide.append(FACTORY_MANAGER.createComponent(XMLTags.TEXT.getTag(), 1, "The Java presentation.Presentation Tool"));
+        slide.append(FACTORY_MANAGER.createComponent(XMLTags.TEXT.getTag(), 2, "Copyright (c) 1996-2000: Ian Darwin"));
+        slide.append(FACTORY_MANAGER.createComponent(XMLTags.TEXT.getTag(), 2, "Copyright (c) 2000-now: Gert Florijn and Sylvia Stuurman"));
+        slide.append(FACTORY_MANAGER.createComponent(XMLTags.TEXT.getTag(), 4, "Running JabberPoint without a filename shows this demo"));
+        slide.append(FACTORY_MANAGER.createComponent(XMLTags.TEXT.getTag(), 1, "Navigation:"));
+        slide.append(FACTORY_MANAGER.createComponent(XMLTags.TEXT.getTag(), 3, "Next slide: PgDn or Enter"));
+        slide.append(FACTORY_MANAGER.createComponent(XMLTags.TEXT.getTag(), 3, "Previous slide: PgUp or up-arrow"));
+        slide.append(FACTORY_MANAGER.createComponent(XMLTags.TEXT.getTag(), 3, "Quit: q or Q"));
+
         return slide;
     }
 
@@ -49,13 +52,13 @@ public class DemoPresentationLoader extends Accessor
     {
         Slide slide = new Slide();
         slide.setTitle("Demonstration of levels and styles");
-        slide.append(1, "Level 1");
-        slide.append(2, "Level 2");
-        slide.append(1, "Level 1 again");
-        slide.append(1, "Level 1 uses style number 1");
-        slide.append(2, "Level 2 uses style number 2");
-        slide.append(3, "This is what level 3 looks like");
-        slide.append(4, "And this is level 4");
+        slide.append(FACTORY_MANAGER.createComponent(XMLTags.TEXT.getTag(), 1, "Level 1"));
+        slide.append(FACTORY_MANAGER.createComponent(XMLTags.TEXT.getTag(), 2, "Level 2"));
+        slide.append(FACTORY_MANAGER.createComponent(XMLTags.TEXT.getTag(), 1, "Level 1 again"));
+        slide.append(FACTORY_MANAGER.createComponent(XMLTags.TEXT.getTag(), 1, "Level 1 uses style number 1"));
+        slide.append(FACTORY_MANAGER.createComponent(XMLTags.TEXT.getTag(), 2, "Level 2 uses style number 2"));
+        slide.append(FACTORY_MANAGER.createComponent(XMLTags.TEXT.getTag(), 3, "This is what level 3 looks like"));
+        slide.append(FACTORY_MANAGER.createComponent(XMLTags.TEXT.getTag(), 4, "And this is level 4"));
         return slide;
     }
 
@@ -63,11 +66,12 @@ public class DemoPresentationLoader extends Accessor
     {
         Slide slide = new Slide();
         slide.setTitle("The third slide");
-        slide.append(1, "To open a new presentation,");
-        slide.append(2, "use File -> Open from the menu.");
-        slide.append(1, " ");
-        slide.append(1, "This is the end of the demo presentation.");
-        slide.append(new BitmapItem(1, "JabberPoint.jpg"));
+        slide.append(FACTORY_MANAGER.createComponent(XMLTags.TEXT.getTag(), 1, "To open a new presentation"));
+        slide.append(FACTORY_MANAGER.createComponent(XMLTags.TEXT.getTag(), 2, "use File -> Open from the menu."));
+        slide.append(FACTORY_MANAGER.createComponent(XMLTags.TEXT.getTag(), 1, " "));
+        slide.append(FACTORY_MANAGER.createComponent(XMLTags.TEXT.getTag(), 1, "This is the end of the demo presentation."));
+        slide.append(FACTORY_MANAGER.createComponent(XMLTags.IMAGE.getTag(), 1, "Jabberpoint.jpg"));
+
         return slide;
     }
 }
